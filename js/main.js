db414271d6f10b2fd0d566582ccce74a7c906195
bablu0k <sreedevikalavakuri919@gmail.com>
2025-02-28 19:31:29 +0530

    new start 2.0

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+document.querySelector('.centered-image').addEventListener('click', function() {
+    // Trigger the animation by adding a class
+    this.classList.add('shutter-effect');
+
+    // Remove the class after the animation ends to reset it
+    setTimeout(() => {
+        this.classList.remove('shutter-effect');
+    }, 1000); // Matches the duration of the animation (1 second)
+});
