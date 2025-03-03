8231a13ec213acb96359e3334fec991f9f9b5fd9
bablu0k <sreedevikalavakuri919@gmail.com>
2025-03-03 22:05:27 +0530

    asdfa

--- static/js/main.js
+++ static/js/main.js
@@ -9,9 +9,4 @@ document.querySelector('.centered-image').addEventListener('click', function() {
     }, 1000); // Matches the duration of the animation (1 second)
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("centered-image").addEventListener("dblclick", function () {
-        window.location.href = "static/html/me.html"; 
-    });
-});
 
